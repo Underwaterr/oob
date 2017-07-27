7129d381a311003c6a0b0c9f61d76347fb84cd40
Tyler Lane <tylerl@eurekasoft.com>
2017-07-27 17:08:38 -0500

    Huh

--- tests/submission.api.test.js
+++ tests/submission.api.test.js
@@ -1,28 +1,10 @@
-let test            = require('tape'),
-    sinon           = require('sinon'),
-    mongoose        = require('mongoose'),
-    sinonMongoose   = require('sinon-mongoose')
+const test = require('tape')
+const proxyquire = require('proxyquire')
 
-
-// USE THIS
-// https://github.com/thlorenz/proxyquire
-
-
-var api = require('../api/submission.api')
-
-test('I am not a clever man', (t)=> {
-
-    var Submission = mongoose.model('Submission')
-    var SubmissionMock = sinon.mock(Submission)
-
-    SubmissionMock
-        .find()
-        .exec()
-        .then(function(result) {
-            console.log("Result:", result)
-        }
-
-    console.log(thing)
-
-    t.end()
+const api = proxyqurie('./api/submission'. {
+    './submission.model': {
+        
+    }
 })
+
+test('Get submission')
